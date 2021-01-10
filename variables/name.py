6d24ab79fba5e9cb27dda7308f8ba5532714434c
babynicth <nichada.th@ku.th>
2021-01-10 10:51:56 +0700

    add name

---
+++ variables/name.py
@@ -0,0 +1,13 @@
+#สามารถประกาศได้
+
+myvar = "meaw"
+my_var = 'meaw'
+_my_var = 'meaw'
+myVar = 'meaw'
+MYVAR = 'meaw'
+myvar2 ='meaw'
+
+#ไม่สามารถประกาศได้
+2myvar = 'meaw'
+my-var = 'meaw'
+my var = 'meaw'
